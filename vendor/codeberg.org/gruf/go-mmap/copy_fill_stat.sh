04d7b9e7be4b1043ac3361c132d55ae6edc10476
kim <grufwub@gmail.com>
2025-11-05 21:58:38 +0100

    [chore] update dependencies (#4542)
    - github.com/minio/minio-go/v7: v7.0.95 -> v7.0.97
    - github.com/ncruces/go-sqlite3: v0.29.1 -> v0.30.0
    - github.com/tdewolff/minify/v2: v2.24.5 -> v2.24.6
    - codeberg.org/gruf/go-mmap: fixes build for BSD platforms
    
    Reviewed-on: https://codeberg.org/superseriousbusiness/gotosocial/pulls/4542
    Co-authored-by: kim <grufwub@gmail.com>
    Co-committed-by: kim <grufwub@gmail.com>

---
+++ vendor/codeberg.org/gruf/go-mmap/copy_fill_stat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+for file in "$(go env GOROOT)"/src/os/stat_{linux,netbsd,openbsd,freebsd}.go; do
+    cp "$file" .
+    name="$(basename "$file")"
+    sed -i "./${name}" -E -e 's|package os|package mmap|' \
+                          -e 's|internal/filepathlite|path|' \
+                          -e 's|filepathlite|path|g' \
+                          -e 's| FileMode| os.FileMode|g' \
+                          -e 's| Mode| os.Mode|g' \
+                          -e 's| FileInfo| os.FileInfo|g'
+    gofmt -w "./${name}"; goimports -w "./${name}"
+done
--- vendor/codeberg.org/gruf/go-mmap/fs.go
+++ vendor/codeberg.org/gruf/go-mmap/fs.go
@@ -2,23 +2,24 @@ package mmap
 
 import (
 	"io/fs"
-	"path"
 	"syscall"
 	"time"
 )
 
 type fileStat struct {
-	syscall.Stat_t
-	name string
-	mode fs.FileMode
+	name    string
+	size    int64
+	mode    fs.FileMode
+	modTime time.Time
+	sys     syscall.Stat_t
 }
 
-func (s *fileStat) Name() string       { return s.name }
-func (s *fileStat) IsDir() bool        { return s.mode.IsDir() }
-func (s *fileStat) Mode() fs.FileMode  { return s.mode }
-func (s *fileStat) Size() int64        { return s.Stat_t.Size }
-func (s *fileStat) ModTime() time.Time { return time.Unix(s.Stat_t.Mtim.Unix()) }
-func (s *fileStat) Sys() any           { return &s.Stat_t }
+func (fs *fileStat) Name() string       { return fs.name }
+func (fs *fileStat) Size() int64        { return fs.size }
+func (fs *fileStat) IsDir() bool        { return fs.mode.IsDir() }
+func (fs *fileStat) Mode() fs.FileMode  { return fs.mode }
+func (fs *fileStat) ModTime() time.Time { return fs.modTime }
+func (fs *fileStat) Sys() any           { return &fs.sys }
 
 // open is a simple wrapper around syscall.Open().
 func open(filepath string, mode int, perm uint32) (fd int, err error) {
@@ -33,38 +34,12 @@ func open(filepath string, mode int, perm uint32) (fd int, err error) {
 func stat(filepath string) (*fileStat, error) {
 	var stat fileStat
 	err := retryOnEINTR(func() error {
-		return syscall.Stat(filepath, &stat.Stat_t)
+		return syscall.Stat(filepath, &stat.sys)
 	})
 	if err != nil {
 		return nil, err
 	}
-	stat.name = path.Base(filepath)
-	stat.mode = fs.FileMode(stat.Stat_t.Mode & 0777)
-	switch stat.Stat_t.Mode & syscall.S_IFMT {
-	case syscall.S_IFBLK:
-		stat.mode |= fs.ModeDevice
-	case syscall.S_IFCHR:
-		stat.mode |= fs.ModeDevice | fs.ModeCharDevice
-	case syscall.S_IFDIR:
-		stat.mode |= fs.ModeDir
-	case syscall.S_IFIFO:
-		stat.mode |= fs.ModeNamedPipe
-	case syscall.S_IFLNK:
-		stat.mode |= fs.ModeSymlink
-	case syscall.S_IFREG:
-		// nothing to do
-	case syscall.S_IFSOCK:
-		stat.mode |= fs.ModeSocket
-	}
-	if stat.Stat_t.Mode&syscall.S_ISGID != 0 {
-		stat.mode |= fs.ModeSetgid
-	}
-	if stat.Stat_t.Mode&syscall.S_ISUID != 0 {
-		stat.mode |= fs.ModeSetuid
-	}
-	if stat.Stat_t.Mode&syscall.S_ISVTX != 0 {
-		stat.mode |= fs.ModeSticky
-	}
+	fillFileStatFromSys(&stat, filepath)
 	return &stat, nil
 }
 
